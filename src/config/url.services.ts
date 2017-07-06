e830f1f3ab6290ebbf79655f5ca4c73093f3a951
Guillermo <guillermoantonio8a@gmail.com>
2017-07-06 00:23:01 -0430

    revision2

--- src/config/url.services.ts
+++ src/config/url.services.ts
@@ -1,3 +1,3 @@
-//export const URL_SERVICIOS = "http://192.168.0.103/localhost/Sitios_web/app_transito/public/api/";
+export const URL_SERVICIOS = "http://192.168.0.103/app_transito/public/api/";
 //192.168.0.103/localhost/Sitios_web/app_transito/public/api/
-export const URL_SERVICIOS = "http://localhost:8080/Sitios_web/infracciones/app_transito/public/api/";
+//export const URL_SERVICIOS = "http://localhost:8080/Sitios_web/infracciones/app_transito/public/api/";
